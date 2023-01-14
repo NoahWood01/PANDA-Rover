9ec5222f14dadf780a366151629c84c91f6bd727
noahwood01 <noahdanielwood@gmail.com>
2023-01-14 15:09:43 -0600

    basic cv for open box

---
+++ src/test.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+import numpy as np
+import sys
+import time
+
+from cv import detect_qr_code, template_match
+
+
+# print(cv.__version__)
+
+test_img = 'samples/box_qr_1.png'
+detect_qr_code(test_img)
+
+test_temp_image = "samples/test_image0.png"
+test_temp = "templates/temp_blue_open.png"
+template_match(test_temp_image, test_temp)
