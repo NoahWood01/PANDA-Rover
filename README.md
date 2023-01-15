7281b759e6421a022da5a78206d3f9e4b07c1c9a
noahwood01 <noahdanielwood@gmail.com>
2023-01-14 22:23:59 -0600

    run script and adjust cv

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PANDA-Rover
+
+`./run.sh start` to start
+
+`./run.sh test` to run test.py
---
+++ src/main.py
@@ -0,0 +1,9 @@
+"""
+This will be the starting point of python scripts / cv.
+"""
+
+def run():
+    pass
+
+if __name__ == "__main__":
+    run()
