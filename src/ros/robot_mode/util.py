d704515732594b628af30b5ec6092b5adeedf7ea
PandaRover <teampandarover@gmail.com>
2023-04-16 19:00:15 -0500

    "4 16 615pm"

--- src/ros/robot_mode/util.py
+++ src/ros/robot_mode/util.py
@@ -145,6 +145,46 @@ def check_if_aligned_with_opening_in_cone(controller):
         return True 
     return False
 
+def check_final_alignment(controller):
+    index_cone_window = 20
+    right_left_error_window = 20
+
+    if controller.front_lidar_scan[(len(controller.front_lidar_scan) / 2)] < 900:
+        return False
+
+    # Checking left edge
+    left_index = float(0)
+    right_index = float(0)
+    right_distance = float(0)
+    left_distance = float(0)
+    scans = list(controller.front_lidar_scan)
+
+    for index, distance in enumerate(scans):
+        if distance < 500:
+            right_index = index
+            right_distance = distance
+            break
+
+    for index in range(len(scans) -1, 0, -1):
+        if scans[index] < 500:
+            left_index = index
+            left_distance = scans[index]
+            break
+
+    print("left_index: ", str(left_index), "right_index: ", str(right_index), "left_distance: ", left_distance, "right_distance: ", right_distance)
+    print("mid distance", str(controller.front_lidar_scan[(len(controller.front_lidar_scan) / 2)]), "left vision distance: ", str(controller.front_lidar_scan[(len(controller.front_lidar_scan) / 2) + index_cone_window]), "right vision distance: ", str(controller.front_lidar_scan[(len(controller.front_lidar_scan) / 2) - index_cone_window]))
+    if (
+        
+        # left_index > (len(controller.front_lidar_scan) / 2) and
+        # right_index < (len(controller.front_lidar_scan) / 2) and
+        abs(left_distance - right_distance) < right_left_error_window and
+        controller.front_lidar_scan[(len(controller.front_lidar_scan) / 2) + index_cone_window] > 500 and
+        controller.front_lidar_scan[(len(controller.front_lidar_scan) / 2) - index_cone_window] > 500
+    ):
+        print("Aligned and rdy to move")
+        return True 
+    return False
+
 def make_opening_aligned(controller):
     while not check_if_aligned_with_opening_in_cone(controller):
         controller.movement_calculator.move_left(speed_percentage=0.1,time_in_ms=ITERATION_TIME)
@@ -160,20 +200,23 @@ def make_opening_aligned(controller):
         if min_scan > 300:
             controller.movement_calculator.move_forward(speed_percentage=0.2,time_in_ms=ITERATION_TIME)
         
-    """
-    print('Detected some opening')
-    while not check_if_aligned_with_opening(controller):
+    
+    while not check_final_alignment(controller):
         controller.movement_calculator.move_left(speed_percentage=0.1,time_in_ms=ITERATION_TIME)
         box_angle_offset = get_angle_offset_of_closest_box(controller)
+        print(box_angle_offset)
         if box_angle_offset > 5:
-            controller.movement_calculator.rotate_clockwise(abs(box_angle_offset))
-        if box_angle_offset < 5:
             controller.movement_calculator.rotate_counterclockwise(abs(box_angle_offset))
+        if box_angle_offset < 5:
+            controller.movement_calculator.rotate_clockwise(abs(box_angle_offset))
+        reached_distance, min_scan =_scan_iteration(controller)
         if min_scan < 200:
             controller.movement_calculator.move_backward(speed_percentage=0.2,time_in_ms=ITERATION_TIME)
         if min_scan > 300:
             controller.movement_calculator.move_forward(speed_percentage=0.2,time_in_ms=ITERATION_TIME)
     
+    controller.movement_calculator.move_forward(speed_percentage=0.2,time_in_ms=ITERATION_TIME)
+    """
     ANGLE_OFFSET_WINDOW = 10
     while not check_if_aligned_with_opening(controller):
         controller.movement_calculator.move_left(speed_percentage=0.1,time_in_ms=ITERATION_TIME)
